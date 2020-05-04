59e09dd84145dd34a05929928f925e734dca5f6a
equetzal <eithnegomez@hotmail.com>
2020-05-04 11:19:44 -0500

    template

---
+++ z.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+using namespace std;
+
+typedef long long int lli;
+
+int main(){
+
+
+	return 0;
+}
